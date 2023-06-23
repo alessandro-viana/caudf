ac1a8f9d4b4950493781fa61133edd8434b1c758
Alessandro Viana <alessandro.viana@gmail.com>
2023-06-23 12:07:18 -0300

    Implemtacao do relacionamento ManyToMany entre as tabelas tb_architect e Tb_category_activities

---
+++ src/main/java/br/gov/caudf/sistemas/entities/Architect.java
@@ -0,0 +1,126 @@
+package br.gov.caudf.sistemas.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_architect")
+public class Architect implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cau;
+	private String description;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_architect_categoryActivities",
+			joinColumns = @JoinColumn(name = "architect_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoryActivities_id")
+			)
+	Set<CategoryActivities> categories = new HashSet<>();
+	
+	public Architect() {
+	}
+
+	public Architect(Long id, String name, String cau, String description, String imgUrl, Instant date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cau = cau;
+		this.description = description;
+		this.imgUrl = imgUrl;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCau() {
+		return cau;
+	}
+
+	public void setCau(String cau) {
+		this.cau = cau;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Set<CategoryActivities> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Architect other = (Architect) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
