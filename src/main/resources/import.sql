cb0b8b2977a2fcb48a2cbd4f17d7d1631768c2c8
Alessandro Viana <alessandro.viana@gmail.com>
2023-06-27 12:17:05 -0300

    Implementacao do ORM mapping na tb_user e tb_role, e fizemos o database seeding dessas tabelas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,13 @@
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Alessandro', 'Viana', 'alessandro@caudf.gov.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Luciana', 'Vieira', 'luciana@caudf.gov.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO tb_category_activities (name, created_At) VALUES ('Arquitetura Ambiental', NOW());
 INSERT INTO tb_category_activities (name, created_At) VALUES ('Neuro Arquitetura', NOW());
 INSERT INTO tb_category_activities (name, created_At) VALUES ('Arquitetura Social', NOW());
