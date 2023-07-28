a2424715e73f1e5b56b8f08d99cab6053860117a
Alessandro Viana <alessandro.viana@gmail.com>
2023-07-28 17:10:02 -0300

    Criação da Entidade/Atributos de Denunciado Denounced

---
+++ src/main/java/br/gov/caudf/sistemas/entities/Denounced.java
@@ -0,0 +1,110 @@
+package br.gov.caudf.sistemas.entities;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_denounced")
+public class Denounced implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String telephone;
+	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+
+	public Denounced() {
+	}
+
+	public Denounced(Long id, String name, String telephone, String imgUrl, String description, Instant date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.telephone = telephone;
+		this.imgUrl = imgUrl;
+		this.description = description;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Denounced other = (Denounced) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
