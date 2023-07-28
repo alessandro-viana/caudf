60fe4265c903c775f9e2de4ae562ead069d8a7c6
Alessandro Viana <alessandro.viana@gmail.com>
2023-07-28 16:55:16 -0300

    Criação da Entidade/Atributos de Denunciante Denouncer

---
+++ src/main/java/br/gov/caudf/sistemas/entities/Denouncer.java
@@ -0,0 +1,103 @@
+package br.gov.caudf.sistemas.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_denouncer")
+public class Denouncer implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String telephone;
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date; 
+
+	public Denouncer() {
+	}
+	
+	public Denouncer(Long id, String name, String telephone, String description, Instant date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.telephone = telephone;
+		this.description = description;
+		this.date = date;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Denouncer other = (Denouncer) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
