2e358d0b25ee5df53479bd7284687700a3865302
Alessandro Viana <alessandro.viana@gmail.com>
2023-07-31 16:47:11 -0300

    Implementação do método delete na Resource e Service Classe Complait

--- src/main/java/br/gov/caudf/sistemas/resources/ComplaintResource.java
+++ src/main/java/br/gov/caudf/sistemas/resources/ComplaintResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,12 @@ public class ComplaintResource {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/br/gov/caudf/sistemas/services/ComplaintService.java
+++ src/main/java/br/gov/caudf/sistemas/services/ComplaintService.java
@@ -1,12 +1,16 @@
 package br.gov.caudf.sistemas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.caudf.sistemas.dto.ComplaintDTO;
 import br.gov.caudf.sistemas.entities.Complaint;
 import br.gov.caudf.sistemas.repositories.ComplaintRepository;
+import br.gov.caudf.sistemas.services.exceptions.DatabaseException;
+import br.gov.caudf.sistemas.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ComplaintService {
@@ -14,13 +18,23 @@ public class ComplaintService {
 	@Autowired
 	private ComplaintRepository repository;
 	
-	
 	@Transactional
 	public ComplaintDTO insert(ComplaintDTO dto) {
 		Complaint entity = new Complaint();
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ComplaintDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Esse ID não existe no banco de dados delete " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de Integridado do Banco de Dados");
+		}
+		
 	}
 	
 	private void copyDtoToEntity(ComplaintDTO dto, Complaint entity) {
